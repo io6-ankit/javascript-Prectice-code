ec7157e8da34caa9b6ceedd8c6251862c2e63e64
io6-ankit <156927710+io6-ankit@users.noreply.github.com>
2024-09-10 15:52:05 +0530

    Check the variable

---
+++ index.js
@@ -0,0 +1,8 @@
+// var _myName = "Ankit";
+// var _1my__Name = "maurya";
+var 1myName ="Maurya";
+// var $myName ="AnkitMaurya";
+// console.log(_1my__Name);
+console.log(1myName);
+// console.log($myName);
+// console.log(_myName);
