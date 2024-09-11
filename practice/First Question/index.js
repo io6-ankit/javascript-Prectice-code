be148cb04faee6e4d21adc90d99516a38582afb2
io6-ankit <156927710+io6-ankit@users.noreply.github.com>
2024-09-11 14:50:42 +0530

    all practice code commit

---
+++ practice/First Question/index.js
@@ -0,0 +1,19 @@
+//First Question
+// var _myName = "Ankit";
+// console.log(_myName);
+// Answer=Ankit
+
+//Second Question
+// var _1myName = "Maurya";
+// console.log(_1myName);
+//Answer=Error(_1myName is not defined)
+
+//Third Question
+// var _my__Name = "Ankit Maurya";
+// console.log(_my__Name);
+//Answer=Ankit Maurya
+
+//Fourth Question
+var $myName = "My Name Is Ankit Maurya";
+console.log($myName);
+//Answer=My Name Is Ankit Maurya
---
+++ practice/Second Question/Second Question/index.js
@@ -0,0 +1,44 @@
+// First Question
+10 + "20";
+console.log(10 + "20");
+//Answer=1020
+// Second Question
+9 - "5";
+console.log(9 - "5");
+//Answer=4
+// Third Question
+"Java" + "Script";
+console.log("Java" + "Script");
+//Answer=JavaScript
+// Fourth Question
+"" + "";
+console.log("" + "");
+//Answer=(   )
+// Five Question
+"" + 0;
+console.log("" + 0);
+//Answer=0
+// Six Question
+"Ankit" - "Maurya";
+console.log("Ankit" - "Maurya");
+//Answer=NaN
+// Seven Question
+"True" + "True";
+console.log("True" + "True");
+//Answer=TrueTrue
+// Eight Question
+true + true;
+console.log(true + true);
+//Answer=2
+// Nine Question
+false + true;
+console.log(false + true);
+//Answer=1
+// Tenth Question
+false - true;
+console.log(false - true);
+//Answer=-1
+// Eleven Question
+true - false;
+console.log(true - false);
+//Answer=1
