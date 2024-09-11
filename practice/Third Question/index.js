eeaa988df8bd531f2fb7539c7f4a579c7b611ce7
io6-ankit <156927710+io6-ankit@users.noreply.github.com>
2024-09-11 16:45:30 +0530

    practice code

---
+++ practice/Third Question/index.js
@@ -0,0 +1,20 @@
+//First Question
+NaN === NaN;
+console.log(NaN === NaN);
+//Answer=False
+//Second Question
+Number.NaN === NaN;
+console.log(Number.NaN === NaN);
+//Answer=False
+//Third Question
+isNaN(NaN);
+console.log(isNaN(NaN));
+//Answer=True
+//Fourth Question
+isNaN(Number.NaN);
+console.log(isNaN(Number.NaN));
+//Answer=True
+//Five Question
+Number.isNaN(NaN);
+console.log(Number.isNaN(NaN));
+//Answer=True
