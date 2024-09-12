0ccdfeabaa31868d7a7d7d4490b6e6937cb367d0
io6-ankit <156927710+io6-ankit@users.noreply.github.com>
2024-09-12 14:18:00 +0530

    all code commit

---
+++ practice/Fourth Question/index.js
@@ -0,0 +1,31 @@
+// 1.What will be the  output of 3**3?
+//2.What will be the output,When we add a number and a string?
+//3.Write a program to swap to number?
+//4.Write a program to swap two numbers without using third variable?
+
+//First Answer=
+console.log(3 ** 3);
+//output=27
+//Second Answer=
+console.log(5 + "Ankit");
+//output=5Ankit
+//Third Answer=
+var a = 30;
+var b = 40;
+var c = a;
+var a = b;
+var b = c;
+console.log("a=", a);
+//output=40
+console.log("b=", b);
+//output=30
+//Fourth Answer=
+var a = 10;
+var b = 20;
+var a = a + b;
+var b = a - b;
+var a = a - b;
+console.log("a   =", a);
+//output=20
+console.log("b=", b);
+//output=10
