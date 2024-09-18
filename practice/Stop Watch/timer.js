8c7aabb497ec2f4be10082783beaa2a19e490456
io6-ankit <156927710+io6-ankit@users.noreply.github.com>
2024-09-18 17:22:25 +0530

    Timer Watch

--- practice/Stop Watch/timer.js
+++ practice/Stop Watch/timer.js
@@ -13,14 +13,16 @@
 //   var d = new Data();
 //   document.querySelector(".watch").innerHTML = d.toLocaleTimeString();
 // }
-let num = 0;
-let count = 0;
-let timer = false;
-function handleClick() {
+var min = 0;
+var hr = 0;
+// var sec = 0;
+var sec = 0;
+var timer = false;
+function handleClickStart() {
   timer = true;
   stopwatch();
 }
-handleClick();
+handleClickStart();
 function handleClickStop() {
   timer = false;
   stopwatch();
@@ -29,16 +31,53 @@ handleClickStop();
 function handleClickReset() {
   timer = false;
   if (timer == false) {
-    count = 0;
-    document.getElementById("count").innerHTML = count;
+    sec = 0;
+    hr = 0;
+    // sec = 0;
+    min = 0;
+    document.getElementById("hr").innerHTML = "00";
+    document.getElementById("min").innerHTML = "00";
+    // document.getElementById("sec").innerHTML = "00";
+    document.getElementById("sec").innerHTML = "00";
   }
   handleClickReset();
 }
 function stopwatch() {
   if (timer == true) {
-    count = count + 1;
-    document.getElementById("count").innerHTML = count;
-    setTimeout("stopwatch()", 100);
+    sec = sec + 1;
+    // if (sec == 100) {
+    //   sec = sec + 1;
+    //   sec = 0;
+    // }
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+    if (min == 60) {
+      hr = hr + 1;
+      min = 0;
+    }
+    var hrString = hr;
+    var minString = min;
+    // var secString = sec;
+    var secString = sec;
+    if (hr < 10) {
+      hrString = "0" + hr;
+    }
+    if (min < 10) {
+      minString = "0" + min;
+    }
+    if (secString < 10) {
+      secString = "0" + sec;
+    }
+    // if (sec < 10) {
+    //   secString = "0" + sec;
+    // }
+    document.getElementById("hr").innerHTML = hrString;
+    document.getElementById("min").innerHTML = minString;
+    // document.getElementById("sec").innerHTML = secString;
+    document.getElementById("sec").innerHTML = secString;
+    setTimeout("stopwatch()", 1000);
   }
 }
 stopwatch(handleClick);
